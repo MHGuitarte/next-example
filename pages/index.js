78ad9815184dd3fc74b0e7cb49af412355fb341c
MHGuitarte <guimhur@gmail.com>
2020-07-14 08:59:13 +0200

    feat(): add get/post functionality for index.js

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,86 @@
-import Head from 'next/head'
+import { useState } from "react";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const movies = await prisma.movie.findMany();
+  return { props: { movies } };
+};
+
+const Home = ({ movies }) => {
+  const [{ director, movieName, yearReleased }, setData] = useState({
+    director: "",
+    movieName: "",
+    yearReleased: 0,
+  });
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+    <div>
+      <form
+        onSubmit={() => {
+          fetch("http://localhost:3000/api/movies", {
+            method: "POST",
+            body: JSON.stringify({
+              director: director,
+              movieName: movieName,
+              yearReleased: Number(yearReleased),
+            }),
+          });
+        }}
+      >
+        <label htmlFor="director-input">Director</label>
+        <input
+          type="text"
+          name="director-input"
+          onChange={(e) =>
+            setData({
+              director: e.target.value,
+              movieName: movieName,
+              yearReleased: yearReleased,
+            })
           }
-        }
-      `}</style>
+        />
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+        <label htmlFor="name-input">Título</label>
+        <input
+          type="text"
+          name="name-input"
+          id=""
+          onChange={(e) =>
+            setData({
+              director: director,
+              movieName: e.target.value,
+              yearReleased: yearReleased,
+            })
+          }
+        />
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+        <label htmlFor="year-input">Año de lanzamiento</label>
+        <input
+          type="text"
+          name="year-input"
+          id=""
+          onChange={(e) =>
+            setData({
+              director: director,
+              movieName: movieName,
+              yearReleased: e.target.value,
+            })
+          }
+        />
+        <button type="submit">Añadir</button>
+      </form>
+      <div className="container">
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <p>Name: {movie.movieName}</p>
+            <p>Director: {movie.director}</p>
+            <p>Year Released: {movie.yearReleased}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
